abf9d332507b0e483984db563e5f1da634e47512
Rohan Punnoose <rohanjp@stanford.edu>
2020-06-18 12:09:57 -0400

    add jupytext cmds

--- .bash
+++ .bash
@@ -23,3 +23,34 @@ fjobs()
 
 alias fj="fjobs"
 
+jupinit()
+{
+    if [ "$#" -ne 2 ]; then
+        echo "Wrong args. Example: \$ jupinit dynamics jl"
+        return
+    fi
+
+    if [ "$2" == "jl" ]; then
+        KERNEL=julia-1.4
+    elif [ "$2" == "py" ]; then
+        KERNEL=python3
+    else
+        echo "Unrecognized file extension/kernelspec"
+        return
+    fi
+
+    touch $1.$2
+    jupytext --to notebook $1.$2
+    jupytext --set-formats ipynb,$2 $1.$2
+    jupytext --set-kernel $KERNEL $1.$2
+    echo "Created $1.$2 and $1.ipynb"
+}
+
+jupopen()
+{
+    FILEPATH=$(realpath $1 --relative-to="$HOME/develop")
+    URL=http://localhost:8888/tree/$FILEPATH
+    open $URL
+}
+
+complete -f -X '!*.ipynb' jupopen
